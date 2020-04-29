17dca39fcf7800d27fb307f6a92764e3a2205fd9
Cole Robertson <cbjrobertson@gmail.com>
2020-04-29 11:13:34 +0100

    updated example

--- reproducible_example.R
+++ reproducible_example.R
@@ -1,10 +1,10 @@
-data = read.csv("https://raw.githubusercontent.com/cbjrobertson/data_share/master/data.csv")
+data <- read.csv("https://raw.githubusercontent.com/cbjrobertson/data_share/master/data.csv")
 
-mod1 = glmer(y ~ poly(x1,2) + poly(x2,2) + (1|group),family=binomial,data=data)
+mod1 <- glmer(y ~ poly(x1,2) + poly(x2,2) + (1|group),family=binomial,data=data)
 plot_model(mod1,type='pred',terms=c("x2 [all]"))
 
 
-mod2 = glmer(y ~ scale(poly(x1,2)) + scale(poly(x2,2)) + (1|group),family=binomial,data=data)
+mod2 <- glmer(y ~ scale(poly(x1,2)) + scale(poly(x2,2)) + (1|group),family=binomial,data=data)
 plot_model(mod2,type='pred',terms=c("x2 [all]"))
 
 
