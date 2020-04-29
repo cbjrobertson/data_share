9f6ed8c989c4c37d3f2c416a8ff8e66f4ead3c3b
Cole Robertson <cbjrobertson@gmail.com>
2020-04-29 11:01:20 +0100

    data path to git

--- reproducible_example.R
+++ reproducible_example.R
@@ -1,4 +1,4 @@
-data = read.csv("~/Desktop/data_share/data.csv")
+data = read.csv("https://raw.githubusercontent.com/cbjrobertson/data_share/master/data.csv")
 
 mod1 = glmer(y ~ poly(x1,2) + poly(x2,2) + (1|group),family=binomial,data=data)
 plot_model(mod1,type='pred',terms=c("x2 [all]"))
@@ -6,3 +6,5 @@ plot_model(mod1,type='pred',terms=c("x2 [all]"))
 
 mod2 = glmer(y ~ scale(poly(x1,2)) + scale(poly(x2,2)) + (1|group),family=binomial,data=data)
 plot_model(mod2,type='pred',terms=c("x1 [all]"))
+
+
