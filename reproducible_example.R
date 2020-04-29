a8b434b59e142e672a5310551709e47adbea7765
Cole Robertson <cbjrobertson@gmail.com>
2020-04-29 11:11:31 +0100

    update example

--- reproducible_example.R
+++ reproducible_example.R
@@ -5,6 +5,6 @@ plot_model(mod1,type='pred',terms=c("x2 [all]"))
 
 
 mod2 = glmer(y ~ scale(poly(x1,2)) + scale(poly(x2,2)) + (1|group),family=binomial,data=data)
-plot_model(mod2,type='pred',terms=c("x1 [all]"))
+plot_model(mod2,type='pred',terms=c("x2 [all]"))
 
 
